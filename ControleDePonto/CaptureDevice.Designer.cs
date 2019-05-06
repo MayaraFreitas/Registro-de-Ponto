3aec424e57c7ac2e871c0997b45111f5d88dd59e
MayaraFreitas <mah.alvesfreitas@hotmail.com>
2019-05-06 18:40:02 -0300

    criando projeto de captura de fotos por camera do notebook

---
+++ ControleDePonto/CaptureDevice.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ControleDePonto
+{
+    partial class CaptureDevice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbCamera = new System.Windows.Forms.PictureBox();
+            this.BtnCapturar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCamera)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbCamera
+            // 
+            this.pbCamera.Location = new System.Drawing.Point(24, 34);
+            this.pbCamera.Name = "pbCamera";
+            this.pbCamera.Size = new System.Drawing.Size(1032, 534);
+            this.pbCamera.TabIndex = 0;
+            this.pbCamera.TabStop = false;
+            this.pbCamera.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // BtnCapturar
+            // 
+            this.BtnCapturar.Location = new System.Drawing.Point(441, 634);
+            this.BtnCapturar.Name = "BtnCapturar";
+            this.BtnCapturar.Size = new System.Drawing.Size(160, 59);
+            this.BtnCapturar.TabIndex = 1;
+            this.BtnCapturar.Text = "Capturar";
+            this.BtnCapturar.UseVisualStyleBackColor = true;
+            this.BtnCapturar.Click += new System.EventHandler(this.BtnCapturar_Click);
+            // 
+            // CaptureDevice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1093, 705);
+            this.Controls.Add(this.BtnCapturar);
+            this.Controls.Add(this.pbCamera);
+            this.Name = "CaptureDevice";
+            this.Text = "CaptureDevice";
+            this.Load += new System.EventHandler(this.CaptureDevice_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCamera)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbCamera;
+        private System.Windows.Forms.Button BtnCapturar;
+    }
+}
---
+++ ControleDePonto/CaptureDevice.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace ControleDePonto
+{
+    public partial class CaptureDevice : Form
+    {
+        private VideoCaptureDevice videoSource;
+        public CaptureDevice()
+        {
+            InitializeComponent();
+
+            // Capturando Array com todas as câmeras disponíveis
+            FilterInfoCollection videoSources = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            if (videoSources != null && videoSources.Count > 0)
+            {
+                // Capturando primeira câmera
+                // MonikerString é algo semelhante ao ID da câmera
+                videoSource = new VideoCaptureDevice(videoSources[0].MonikerString);
+                videoSource.NewFrame += VideoSource_NewFrame;
+                videoSource.Start();
+            }
+        }
+
+        private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            // Fazer dispose para evitar o consumo de memória
+            if (pbCamera.Image != null)
+            {
+                pbCamera.Image.Dispose();
+            }
+            
+            // Capturar Frame da câmera e exibir no pitcure box
+            pbCamera.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void BtnCapturar_Click(object sender, EventArgs e)
+        {
+            if (pbCamera.Image != null)
+            {
+                try
+                {
+                    // Parar o frame no momento que o click aconteceu
+                    videoSource.NewFrame -= VideoSource_NewFrame;
+
+                    // Abrindo dialog para escolher onde salvar
+                    using (var dialog = new SaveFileDialog())
+                    {
+                        dialog.DefaultExt = "png";
+                        dialog.AddExtension = true; // Adicionando extension automaticamente
+
+                        // Se houve click em OK...
+                        if (dialog.ShowDialog() == DialogResult.OK)
+                        {
+                            pbCamera.Image.Save(dialog.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                        }
+                    }
+                }
+                finally // Independente do que acontecer execute isso
+                {
+                    videoSource.NewFrame += VideoSource_NewFrame;
+                }
+            }
+        }
+
+        // Parar câmera quando o formulário fechar 
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (videoSource.IsRunning)
+            {
+                videoSource.Stop();
+            }
+            base.OnFormClosing(e);
+        }
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CaptureDevice_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
